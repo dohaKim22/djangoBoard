f1482626926a33d60eb8bb439af4d3b07d3087b6
dohaKim22 <kimdoha03@gmail.com>
2023-07-05 00:19:37 +0900

    Initial commit

---
+++ board_app/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .models import Board
+
+# Create your views here.
+def index(request):
+    boards = {'boards': Board.objects.all()}
+    return render(request, 'list.html', boards)
+
+def post(request):
+    if request.method == "POST":
+        author = request.POST['author']
+        title = request.POST['title']
+        content = request.POST['content']
+        board = Board(author=author, title=title, content=content)
+        board.save()
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'post.html')
+
+def detail(request, id):
+    try:
+        board = Board.objects.get(pk=id)
+    except Board.DoesNotExist:
+        raise Http404("Does not exist!")
+    return render(request, 'detail.html', {'board': board})
+
+def edit(request, id):
+    try:
+        board = Board.objects.get(pk=id)
+    except Board.DoesNotExist:
+        raise Http404("게시글이 존재하지 않습니다!")
+
+    if request.method == "POST":
+        board.author = request.POST['author']
+        board.title = request.POST['title']
+        board.content = request.POST['content']
+        board.save()
+        return redirect('detail', id=board.id)
+    else:
+        return render(request, 'edit.html', {'board': board})
+
+def delete(request, id):
+    try:
+        board = Board.objects.get(pk=id)
+        board.delete()
+    except Board.DoesNotExist:
+        raise Http404("Does not exist!")
+    return redirect('index')
